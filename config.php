ef5f6089b932cd15876e16f78d03ab0a5757b961
VadimYesin <34915176+VadimYesin@users.noreply.github.com>
2017-12-28 19:33:30 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://dev3.dreamvention.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://dev3.dreamvention.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/dev3/public_html/catalog/');
+define('DIR_SYSTEM', '/var/www/dev3/public_html/system/');
+define('DIR_LANGUAGE', '/var/www/dev3/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/dev3/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/dev3/public_html/system/config/');
+define('DIR_IMAGE', '/var/www/dev3/public_html/image/');
+define('DIR_CACHE', '/var/www/dev3/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/dev3/public_html/system/storage/download/');
+define('DIR_LOGS', '/var/www/dev3/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/dev3/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/dev3/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'dev3_id108');
+define('DB_PASSWORD', 'mSLPaxW9');
+define('DB_DATABASE', 'dev3');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
